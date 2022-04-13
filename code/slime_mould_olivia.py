62a758f3236ff2f0375fb5506c713607de578250
omasella <omasella99@gmail.com>
2022-04-13 14:02:08 -0400

    Create slime_mould_olivia.py

---
+++ code/slime_mould_olivia.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import matplotlib.patheffects as PathEffects
+
+import ca
+import matplotlib.animation as ani
+import matplotlib.colors as colours
+import matplotlib
+matplotlib.use("TkAgg")
+
