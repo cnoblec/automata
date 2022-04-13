1c3318231e412a3ea02b360ce96e454213d5f11e
Carlos Noble Curveira <cjnoblec@uwaterloo.ca>
2022-04-13 16:12:05 -0400

    Update gol.py

--- code/gol.py
+++ code/gol.py
@@ -6,60 +6,55 @@ import matplotlib.colors
 import numpy as np
 
 class Life(ca.CA):
+
+    patterns =  {
+                "glider":   np.array([  [0,1,0],
+                                        [0,0,1],
+                                        [1,1,1]]
+                                    ),
+                "blinker":  np.array([  [0,1,0],
+                                        [0,1,0],
+                                        [0,1,0]]
+                                    ),
+                "beacon":   np.array([  [1,1,0,0],
+                                        [1,0,0,1],
+                                        [0,0,1,1]]
+                                    ),
+                "pulsar":   np.array([  [0,0,1,1,0,0,0,0,0,1,1,0,0],
+                                        [0,0,0,1,1,0,0,0,1,1,0,0,0],
+                                        [1,0,0,1,0,1,0,1,0,1,0,0,1],
+                                        [1,1,1,0,1,1,0,1,1,0,1,1,1],
+                                        [0,1,0,1,0,1,0,1,0,1,0,1,0],
+                                        [0,0,1,1,1,0,0,0,1,1,1,0,0],
+                                        [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                        [0,0,1,1,1,0,0,0,1,1,1,0,0],
+                                        [0,1,0,1,0,1,0,1,0,1,0,1,0],
+                                        [1,1,1,0,1,1,0,1,1,0,1,1,1],
+                                        [1,0,0,1,0,1,0,1,0,1,0,0,1],
+                                        [0,0,0,1,1,0,0,0,1,1,0,0,0],
+                                        [0,0,1,1,0,0,0,0,0,1,1,0,0]]
+                                    ),
+                }
+
     def __init__(self, shape=None, lattice=None):
-        super().__init__(shape, int, lattice, ca.NEIGHBOURS2D8OP)
+        super().__init__(shape=shape, cell_type=int, lattice=lattice, neighbour_order=ca.NEIGHBOURS2D8OP)
         if shape is not None:
             self.lattice[:] = 0 # set the lattice to be zeros instead of empty
 
     def update(self, old, new, indx):
         cur = old[indx]
-        # indx_n = np.array(self.neighbours(indx)).T
-        # print(indx_n[0], indx_n[1])
-        # print(old)
-        # print(old[indx_n])
+
         cells = 0
         for n_idx in self.neighbours(indx):
             cells += old[n_idx]
-
-        # if cells >= 1:
-        #     print("cur:\t",cur)
-        #     print("cells:\t",cells)
-        #     print("next:\t",(cur and cells >= 2 and cells <= 3) or (not cur and cells == 3))
 
         new[indx] = (cur and cells >= 2 and cells <= 3) or (not cur and cells == 3)
         return
 
     def add_shape(self, indx, pattern: Literal["glider", "blinker", "beacon", "pulsar"], fliph=False, flipv=False, transpose=False):
         # indx of top left corner of pattern block
-        patterns =    {
-                        "glider":   np.array([  [0,1,0],
-                                                [0,0,1],
-                                                [1,1,1]]
-                                            ),
-                        "blinker":  np.array([  [0,1,0],
-                                                [0,1,0],
-                                                [0,1,0]]
-                                            ),
-                        "beacon":   np.array([  [1,1,0,0],
-                                                [1,0,0,1],
-                                                [0,0,1,1]]
-                                            ),
-                        "pulsar":   np.array([  [0,0,1,1,0,0,0,0,0,1,1,0,0],
-                                                [0,0,0,1,1,0,0,0,1,1,0,0,0],
-                                                [1,0,0,1,0,1,0,1,0,1,0,0,1],
-                                                [1,1,1,0,1,1,0,1,1,0,1,1,1],
-                                                [0,1,0,1,0,1,0,1,0,1,0,1,0],
-                                                [0,0,1,1,1,0,0,0,1,1,1,0,0],
-                                                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                                                [0,0,1,1,1,0,0,0,1,1,1,0,0],
-                                                [0,1,0,1,0,1,0,1,0,1,0,1,0],
-                                                [1,1,1,0,1,1,0,1,1,0,1,1,1],
-                                                [1,0,0,1,0,1,0,1,0,1,0,0,1],
-                                                [0,0,0,1,1,0,0,0,1,1,0,0,0],
-                                                [0,0,1,1,0,0,0,0,0,1,1,0,0]]
-                                            ),
-                    }
-        pattern_arr = patterns[pattern]
+        
+        pattern_arr = Life.patterns[pattern]
 
         if pattern_arr.shape > self.lattice.shape:
             raise AssertionError(f"invalid 'pattern' with shape: '{pattern_arr.shape}' will not fit in lattice of size '{self.lattice.shape}'")
