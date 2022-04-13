9b56b4b62a980948bb093c3278f7b3f1c694f267
omasella <omasella99@gmail.com>
2022-04-13 17:31:32 -0400

    Added docstrings

--- code/traffic.py
+++ code/traffic.py
@@ -23,7 +23,7 @@ class Traffic(ca.CA):
         4 : (-1,  0),
     }
 
-    # This dictionary 
+    # This dictionary gives the unicode strings for the directional arrows
     car_arrows = {
         0 : '',
         1 : '\u2192',
@@ -32,14 +32,35 @@ class Traffic(ca.CA):
         4 : '\u2191'
     }
 
-    car_cmap = ListedColormap(['white', 'red','blue','green','purple'])
+    # Colour map for car cells
+    car_cmap = ListedColormap(['white', 'red', 'blue', 'green', 'purple'])
 
     def __init__(self, init_lattice):
-        #self.lattice = init_lattice
+        '''
+        Initialize the Traffic instance
+
+        Parameters:
+            init_lattice: 
+                initial conditions for the lattice featuring a numpy array
+                of integers ranging from 0 to 4 (inclusive)
+        '''
         super().__init__(cell_type = int, lattice = init_lattice)
         self.shape = self.lattice.shape
 
     def update_lattice(self):
+        '''
+        Update the entire lattice by looking at every cell and following these rules:
+            * If a cell has a car in it, check to see if the next cell in its 
+                direction is empty, if so move the car
+            * The "next cell" consists of the neighbouring cell in both the 
+                lattice and a temporary lattice containing all new movements
+            * If a cell has a car in it and cannot move to the next one, it 
+                stays in that original cell
+        This function updates the self.lattice attribute
+
+        Returns:
+            None
+        '''
         temp_lattice = self.empty()
 
         indices = ca.arrayIndexes(self.shape)
@@ -55,8 +76,26 @@ class Traffic(ca.CA):
             else:
                 temp_lattice[i] = ival
         self.lattice = temp_lattice
+        return
 
     def update(self, old, new, indx):
+        '''
+        Update the cell at index indx according to these rules:
+            * If a cell has a car in it, check to see if the next cell in its 
+                direction is empty, if so move the car
+            * The "next cell" consists of the neighbouring cell in both the 
+                lattice and a temporary lattice containing all new movements
+            * If a cell has a car in it and cannot move to the next one, it 
+                stays in that original cell
+
+        Parameters:
+            * old: previous step's lattice
+            * new: current step's lattice (also referred to as temp_lattice)
+            * indx: location of cell to move
+
+        Returns:
+            None
+        '''
         ival = old[indx]
         if ival == 0:
             return
@@ -70,6 +109,18 @@ class Traffic(ca.CA):
         return
 
     def display(self, title=''):
+        '''
+        Displays the lattice
+
+        Parameters:
+            * title: a string for the plot's title
+
+        Returns:
+            * fig: the figure instance
+            * ax: the axes instance
+            * im: the imshow instance
+            * arrows: the list of text arrow labels
+        '''
         fig, ax = plt.subplots(figsize=(10,7))
         im = ax.imshow(self.lattice, cmap=Traffic.car_cmap, vmin=0, vmax=4)
         ax.set_title(title)
@@ -94,6 +145,17 @@ class Traffic(ca.CA):
         return fig, ax, im, arrows
     
     def animation(self, N_steps, title=''):
+        '''
+        Animates the traffic instance over N steps
+
+        Parameters:
+            * N_steps: integer number of steps to run simulation for
+            * title: string representing the title of the plot; 
+                     will be followed by "n steps" where n is the step #
+
+        Returns:
+            Animation of traffic simulation
+        '''
         fig, ax, im, arrows = self.display(title)
 
         def init():
